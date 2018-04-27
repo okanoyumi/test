347a475868c450a5e6d9b2a3dc5cfd3efb273407
Yumi Okano <okano.yumi.101@gmail.com>
2018-04-27 16:48:25 +0900

    yo

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if ( have_posts() )
 while ( have_posts() ) :
   the_post();
 ?>
+<div class="wrapper-s">
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?> class="post-main">
   <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   <ul class="post">
@@ -19,48 +20,11 @@ while ( have_posts() ) :
     <li><?php the_category( ', ' ); ?></li>
     <li><?php the_tags( '' ); ?></li>
   </ul>
-<?php the_post_thumbnail(); ?>
+<div class="post-img"><?php the_post_thumbnail(); ?></div>
 <?php the_content( '読む' ); ?>
-</div>
-</div>
+</div></div>
 <?php endwhile; ?>
+</div>
 
 
-<!-- <section class="toukou1">
-  <h2>投稿</h2>
-  <div class="container">
-    <div class="item">
-      <img src="img/1.jpg">
-      <p class="pull-right">抜粋が
-        ります。抜粋が入ります。抜粋が入ります。</p>
-    </div>
-    <div class="item">
-      <img src="img/1.jpg">
-      <p class="pull-right">抜粋が入ります。抜粋が入ります。抜粋が入ります。</p>
-    </div>
-    <div class="item">
-      <img src="img/1.jpg">
-      <p class="pull-right">抜粋が入ります。抜粋が入ります。抜粋が入ります。</p>
-    </div>
-  </div>
-</section>
-
-<section class="news">
-  <h2>news</h2>
-  <div class="container">
-    <div class="item">
-      <img src="img/n1.jpg">
-      <p class="pull-right">抜粋が入ります。抜粋が入ります。抜粋が入ります。</p>
-    </div>
-    <div class="item">
-      <img src="img/n2.jpg">
-      <p class="pull-right">抜粋が入ります。抜粋が入ります。抜粋が入ります。</p>
-    </div>
-    <div class="item">
-      <img src="img/n3.jpg">
-      <p class="pull-right">抜粋が入り抜粋が入ります。抜粋が入ります。</p>
-    </div>
-
-  </div>
-</section> -->
 <?php get_footer(); ?>
